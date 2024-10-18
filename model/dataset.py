6e98c796ed963c68a8d93701b00ae17d44cf18f7
Seoyeon Oh <sohtks@swu.ac.kr>
2024-10-18 23:34:01 +0900

    add: 메타데이터에 값이 없을 경우 처리

--- model/dataset.py
+++ model/dataset.py
@@ -1,8 +1,10 @@
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
+import os
+import pandas as pd
 
-# Custom Dataset
+# Custom Dataset class for DeepFake dataset
 class DeepFakeDataset(Dataset):
     def __init__(self, dataframe, transform=None):
         self.data = dataframe
@@ -13,18 +15,24 @@ class DeepFakeDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
-        face = Image.open(row['face'])
-        nose = Image.open(row['nose'])
-        central = Image.open(row['central'])
-        left_cheek = Image.open(row['left_cheek'])
-        right_cheek = Image.open(row['right_cheek'])
+        
+        def safe_open_image(path):
+            if pd.isna(path) or not os.path.exists(path):
+                return None
+            return Image.open(path)
+
+        face = safe_open_image(row['face'])
+        nose = safe_open_image(row['nose'])
+        central = safe_open_image(row['central'])
+        left_cheek = safe_open_image(row['left_cheek'])
+        right_cheek = safe_open_image(row['right_cheek'])
 
         if self.transform:
-            face = self.transform(face)
-            nose = self.transform(nose)
-            central = self.transform(central)
-            left_cheek = self.transform(left_cheek)
-            right_cheek = self.transform(right_cheek)
+            face = self.transform(face) if face else None
+            nose = self.transform(nose) if nose else None
+            central = self.transform(central) if central else None
+            left_cheek = self.transform(left_cheek) if left_cheek else None
+            right_cheek = self.transform(right_cheek) if right_cheek else None
 
         label = torch.tensor(1 if row['deepfake'] else 0, dtype=torch.long)
         
