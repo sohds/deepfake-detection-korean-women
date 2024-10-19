7766de8bed38596f8d58b799fe09674081a8e85d
Seoyeon Oh <sohtks@swu.ac.kr>
2024-10-19 13:23:38 +0900

    fix: swintransformer 구조 변경

--- model/model.py
+++ model/model.py
@@ -14,23 +14,21 @@ class XceptionFeatureExtractor(nn.Module):
 
 # Swin Transformer Classifier
 class SwinTransformerClassifier(nn.Module):
-    def __init__(self, input_dim, num_classes=2):
+    def __init__(self, num_classes=2):
         super(SwinTransformerClassifier, self).__init__()
-        self.swin = create_model('swin_base_patch4_window7_224', pretrained=True)
-        self.swin.head = nn.Linear(self.swin.head.in_features, num_classes)
+        self.swin = create_model('swin_base_patch4_window7_224', pretrained=True, num_classes=num_classes)
 
     def forward(self, x):
         return self.swin(x)
 
-# Full Model for each ablation study
+# # Full Model for each ablation study
 class DeepFakeDetectionModel(nn.Module):
     def __init__(self, study_type, num_classes=2):
         super(DeepFakeDetectionModel, self).__init__()
         self.study_type = study_type
         
         if study_type == 'face':
-            self.classifier = SwinTransformerClassifier(2048, num_classes=2)  
-            # Only Swin Transformer without feature extraction
+            self.classifier = create_model('swin_base_patch4_window7_224', pretrained=True, num_classes=num_classes)
         else:
             self.face_extractor = XceptionFeatureExtractor()
             self.secondary_extractor = XceptionFeatureExtractor()
@@ -42,20 +40,24 @@ class DeepFakeDetectionModel(nn.Module):
             else:  # face_cheeks_nose
                 input_dim = 8192  # 2048 (face) + 2048 (left_cheek) + 2048 (nose) + 2048 (right_cheek)
             
-            self.classifier = SwinTransformerClassifier(input_dim, num_classes=2)
+            self.classifier = nn.Linear(input_dim, num_classes)
 
     def forward(self, face, secondary=None):
         if self.study_type == 'face':
             return self.classifier(face)
         else:
             face_features = self.face_extractor(face)
-            secondary_features = self.secondary_extractor(secondary)
-            
-            if self.study_type == 'face_cheeks_nose':
-                combined_features = torch.cat((face_features, secondary_features[:, :2048], 
-                                               secondary_features[:, 2048:4096], 
-                                               secondary_features[:, 4096:]), dim=1)
-            else:
+            if self.study_type == 'face_nose':
+                secondary_features = self.secondary_extractor(secondary)
                 combined_features = torch.cat((face_features, secondary_features), dim=1)
+            elif self.study_type == 'face_central':
+                secondary_features = self.secondary_extractor(secondary)
+                combined_features = torch.cat((face_features, secondary_features), dim=1)
+            elif self.study_type == 'face_cheeks_nose':
+                left_cheek, nose, right_cheek = secondary.chunk(3, dim=1)
+                left_cheek_features = self.secondary_extractor(left_cheek)
+                nose_features = self.secondary_extractor(nose)
+                right_cheek_features = self.secondary_extractor(right_cheek)
+                combined_features = torch.cat((face_features, left_cheek_features, nose_features, right_cheek_features), dim=1)
             
             return self.classifier(combined_features)
